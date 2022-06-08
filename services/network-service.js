00e279e23dca79a19a4cc172121415aab81b9f6e
Matheus Cobalchini <matmathu@gmail.com>
2022-06-08 12:39:32 -0300

    fixed thread concurrency

--- services/network-service.js
+++ services/network-service.js
@@ -472,7 +472,7 @@ exports.Network = {
 
         let startTime = new Date();
 
-        const thread = new PromisePool(promiseProducer, 8);
+        const thread = new PromisePool(promiseProducer, 7);
 
         try {
             App.log(`Started gathering heroes data`);
